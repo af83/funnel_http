adb47305c37a65c9c1b5a045c468bf6dace5e854
chatgris <jboyer@af83.com>
2014-07-02 17:22:42 +0200

    Add name to FunnelHttp.Supervisor
    Signed-off-by: chatgris <julien.boyer@af83.com>

--- lib/funnel_http/supervisor.ex
+++ lib/funnel_http/supervisor.ex
@@ -2,7 +2,7 @@ defmodule FunnelHttp.Supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, [])
+    Supervisor.start_link(__MODULE__, [], name: FunnelHttpSupervisor)
   end
 
   def init([]) do
