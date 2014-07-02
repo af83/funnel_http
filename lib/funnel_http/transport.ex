42b4041681031cae9b6c51f3eee4a8f9902a4238
chatgris <jboyer@af83.com>
2014-07-02 16:27:52 +0200

    Add more info about queries to river response
    Signed-off-by: chatgris <julien.boyer@af83.com>

--- lib/funnel_http/transport.ex
+++ lib/funnel_http/transport.ex
@@ -2,6 +2,15 @@ defimpl Funnel.Transport, for: Elixir.Plug.Conn do
   import Plug.Conn
 
   def write(conn, %{:id => id, :item => item}) do
+    queries = Enum.map(item[:query_ids], &to_query/1)
+    item = %{queries: queries, body: item[:body]}
+    {:ok, item} = JSEX.encode(item)
+
     chunk conn, EventStreamMessage.to_message(id, item)
+  end
+
+  defp to_query(id) do
+    {:ok, _id, metadata} = FunnelHttp.Query.Registry.find(id)
+    %{id: id, metadata: metadata}
   end
 end
